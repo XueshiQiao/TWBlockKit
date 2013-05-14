299f5081594c315f7f98eee2db38300f9283403f
qiaoxueshi <qiaoxueshi@gmail.com>
2013-05-14 23:21:40 +0800

    Revert "Revert "First commit  :)""
    This reverts commit ab182f76459108d4741a0e44632577a74c185231.

---
+++ TWAlertView/TWViewController.h
@@ -0,0 +1,14 @@
+//
+//  TWViewController.h
+//  TWAlertView
+//
+//  Created by Joey on 13-5-14.
+//  Copyright (c) 2013年 Towords. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWViewController : UIViewController
+
+- (IBAction)btnPressed:(id)sender;
+@end
---
+++ TWAlertView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TWAlertView
+//
+//  Created by Joey on 13-5-14.
+//  Copyright (c) 2013年 Towords. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TWAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TWAppDelegate class]));
+    }
+}
